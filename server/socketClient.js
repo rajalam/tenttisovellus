a08f6762e44f3728e1a5ce63750d8cf5457ad1eb
rajalam <mikko.rajala@iki.fi>
2023-01-03 14:28:53 +0200

    Päivitys uuden client esimerkin mukaiseksi->ok

--- server/socketClient.js
+++ server/socketClient.js
@@ -5,35 +5,41 @@ var nimimerkkiAsetettu = false
 var nimimerkki = ""
 
 var client = new net.Socket();
+var rl = null
+
 client.connect(1337, '127.0.0.1', () => {
     console.log('Connected');
- //   client.write('Hello, server! Love, Client.');
-});
-
-client.on('error', (data) => {
-    console.log('Nyt tuli muuten tupen rapinat' + data)
-});
-
-client.on('data', (data) => {
-    console.log('Received: ' + data);
 
     //lue komentoriviltä tekstiä ja lähetä se sockettiin. 
     //    client.write (komentoriviltäluettudata)
 
     //console.log("Nimimerkkiasetettu: ", nimimerkkiAsetettu)
-    if( ! nimimerkkiAsetettu ) {
+    if (!nimimerkkiAsetettu) {
 
         nimimerkki = annaJaLahetaSyote("Syötä nimimerkki: ")
-        
+
         //console.log("Nimimerkki: ", nimimerkki)
         //nimimerkkiAsetettu = true
     } else { //nimimerkki asetettu
-        annaJaLahetaSyote(nimimerkki+' >')
+        annaJaLahetaSyote(nimimerkki + ' >')
     }
 
-    
+});
 
-    
+client.on('error', (data) => {
+    console.log('Nyt tuli muuten tupen rapinat ' + data)
+});
+
+client.on('data', async (data) => {
+    console.log('Received: ' + data);
+
+    if( nimimerkkiAsetettu ) {
+        rl.setPrompt(nimimerkki + ' >');    
+    }
+    rl.prompt()
+
+
+
 });
 
 client.on('close', function () {
@@ -42,32 +48,32 @@ client.on('close', function () {
 
 const annaJaLahetaSyote = (promptString) => {
     //alku oma funktio
-    var rl = readline.createInterface(
+    rl = readline.createInterface(
         process.stdin, process.stdout);
 
     rl.setPrompt(promptString);
-    rl.prompt()
+    //rl.prompt()
 
     rl.on('line', (teksti) => {
         //console.log(`teksti: ${teksti}`);
-        if( nimimerkkiAsetettu ) {
+        if (nimimerkkiAsetettu) {
             client.write(nimimerkki + ': ' + teksti)
         } else { //nimimerkkiä ei asetettu
             client.write(teksti)
         }
-        
-        
-        if( ! nimimerkkiAsetettu ) {
+
+
+        if (!nimimerkkiAsetettu) {
             nimimerkki = teksti
             nimimerkkiAsetettu = true
         }
-        rl.close();
-        
+        //rl.close();
+
         //return teksti
 
     });
 
-    
+
     //    client.write("Viesti asiakkaalta")
     //	client.destroy(); // kill client after server's response
     //loppu oma funktio
